085f44fe4372d3ad6cc8b52708fec1a80f57d8e3
nitrocaster <paul-kv@yandex.ru>
2016-01-16 14:59:02 +0300

    gdiff: Add -p option to diff exact commit, improve usage info.

--- windows/gdiff.cmd
+++ windows/gdiff.cmd
@@ -1,12 +1,25 @@
 @echo off
 if "%1" equ "-h" (
-  echo gdiff: compare modified git files with difftool
-  goto ret
+  goto print_usage
 )
 if "%1" equ "-c" (
   git difftool -y --cached %2
-) else (
-  git difftool -y %1
+  goto ret
 )
+if "%1" equ "-p" (
+  if "%2" neq "" (
+    git difftool -y %2~1 %2
+    goto ret
+  )
+  goto print_usage
+)
+git difftool -y %1
 echo.
+goto ret
+
+:print_usage
+echo gdiff: compare modified git files with difftool
+echo     -h        : show this message
+echo     -c [file] : diff cached (staged) changes
+echo     -p ^<sha^>  : diff exact commit
 :ret
