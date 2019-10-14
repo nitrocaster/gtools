c60cde4b06f168521c1880f298c8efe1693b91a1
Pavel Kovalenko <paul-kv@yandex.ru>
2019-10-14 19:26:58 +0300

    rgrep: Color output and include binary files.

--- windows/rgrep.cmd
+++ windows/rgrep.cmd
@@ -2,5 +2,5 @@
 setlocal
 set exclude_dir=".[^^\.]*",bin,binaries,libraries,lib,intermediate,ipch
 set exclude="*.sdf","*.VC.db",".*"
-grep -rs --exclude-dir={%exclude_dir%} --exclude={%exclude%} %* .
+grep --binary-files=text --color=auto -rs --exclude-dir={%exclude_dir%} --exclude={%exclude%} %* .
 endlocal
